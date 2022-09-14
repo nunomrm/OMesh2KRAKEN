df4e47431bcaae37c92003aff9e2084e5c816154
Nuno Rocha <101452182+nunomrm@users.noreply.github.com>
2022-09-14 16:49:43 +0100

    Update Plotting_Azores.m

--- tests/azores/Plotting_Azores.m
+++ tests/azores/Plotting_Azores.m
@@ -1,18 +1,25 @@
+% This script has the purpose to display plots of Group Velocity field (for January) of of the Azores test case of OMesh2KRAKEN
+%
+% (c) Nuno Monteiro and Tiago Oliveira, University of Aveiro
+% September 2022
+%
+
+
 clear all, close all, clc
 
 dirname='data_kraken/';
-files = dir ([dirname '*.prt']);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Pre-processing %%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+fname_env = 'azores';               % prefix name of all ENV files
+files = dir ([dirname '*.prt']);
+N=length({files.name});             % number of ENV files
 S=shaperead('..\..\data\shorelines\CNTR_RG_01M_2020_4326.shp');
 lo_s=[S.X]; la_s=[S.Y];
 
-N=length({files.name});
-
-N_mod=zeros(N,1);                   % array of the number of modes of all prt files
-N_prt_mod=zeros(N,1);               % array of the printed nr of modes of all prt files
-m=10;                               % number of modes to extract
-Cg = zeros(N,m);                    % matrix of group velocities obtained by KRAKEN (first m modes at each point are extracted)
-
+% Read mesh data (nodes, elements, depth)
 mesh=load('mesh_data.mat');
 z_m=mesh.z;
 lon_m=mesh.lon;
@@ -20,10 +27,22 @@ lat_m=mesh.lat;
 tri=mesh.tri;
 xy_lims=[min(lon_m) max(lon_m) min(lat_m) max(lat_m)];
 
-lo_s=[S.X]; la_s=[S.Y];
+% Initialize Cg matrix for first 10 modes
+N_mod=zeros(N,1);                   % array of the number of modes of all prt files
+N_prt_mod=zeros(N,1);               % array of the printed nr of modes of all prt files
+m=10;                               % number of modes to extract
+Cg = zeros(N,m);                    % matrix of group velocities obtained by KRAKEN (first m modes at each point are extracted)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% end of pre-processing %%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Plotting Group Velocity (Cg) %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 for i=1:N
-    fname = [dirname 'azores_' sprintf( '%06d', i ) '.prt'];
+    fname = [dirname fname_env '_' sprintf( '%06d', i ) '.prt'];
     fid = fopen(fname);
     lines=textread(fname,'%s','delimiter','\n');
     i_m=find(~cellfun(@isempty,strfind(lines,'Number of modes')));
@@ -48,3 +67,7 @@ fill3(lo_s,la_s,ones(1,length(lo_s)).*1e6,'k')
 xlim([xy_lims(1) xy_lims(2)])
 ylim([xy_lims(3) xy_lims(4)])
 saveas(gcf,['plots/Cg_mode_1.png'])
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% end of plotting Cg %%%%5%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
