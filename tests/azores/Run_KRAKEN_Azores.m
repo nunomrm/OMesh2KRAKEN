d40859a477883c12d4b66717eb1f3bafb1a4024d
Nuno Rocha <101452182+nunomrm@users.noreply.github.com>
2022-09-14 16:11:43 +0100

    Update Run_KRAKEN_Azores.m

--- tests/azores/Run_KRAKEN_Azores.m
+++ tests/azores/Run_KRAKEN_Azores.m
@@ -1,17 +1,22 @@
-%% initialize and copy env files to current directory
+% This script runs Acoustics Toolbox's KRAKEN model for the Azores test case of OMesh2KRAKEN
+%
+% (c) Nuno Monteiro and Tiago Oliveira, University of Aveiro
+% September 2022
+%
 
 clear all, close all, clc
 
-addpath(genpath('..\..\atWin10_2020_11_4\windows-bin-20201102')); % to call windows model executables
+addpath(genpath('..\..\atWin10_2020_11_4\windows-bin-20201102')); % addpath to call Windows 10 binaries of Acoustics Toolbox
 
 copyfile('data_kraken/*env','.','f')
 
-%% run kraken
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Run KRAKEN %%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-t0=tic;
+t0=tic;    % Start counting elapsed time of simulation
 
 runkraken = which( 'kraken.exe' );
-
 DirInfo = dir( '*.env' );
 
 for ifile = 1 : length( DirInfo )
@@ -26,7 +31,11 @@ end
 
 elapsedtime=toc(t0) % measured elapsed time to run kraken in seconds
 
-%% delete or move kraken input/output files
-delete *.env % env files are already archived at './data_kraken'
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% end of run KRAKEN %%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Deletion/moving KRAKEN data files
+delete *.env % ENV files are already archived at './data_kraken'
 delete *.mod
-movefile('azores_*.prt','data_kraken\');
+movefile('azores*.prt','data_kraken\');
