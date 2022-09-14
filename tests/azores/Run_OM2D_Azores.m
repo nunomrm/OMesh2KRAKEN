8d3bc15521f450d8d84c8c94ec405d70402709e0
Nuno Rocha <101452182+nunomrm@users.noreply.github.com>
2022-09-14 15:59:12 +0100

    Update Run_OM2D_Azores.m

--- tests/azores/Run_OM2D_Azores.m
+++ tests/azores/Run_OM2D_Azores.m
@@ -1,7 +1,12 @@
-% This script runs the Azores Archipelago example for OMesh2kraken
+% This script runs the Azores Archipelago example for OMesh2KRAKEN
+%
+% (c) Nuno Monteiro and Tiago Oliveira, University of Aveiro
+% September 2022
+%
+
 clear all, close all, clc
 
-% addpaths
+% Add paths to call OceanMesh2D routines, load data, etc.
 addpath(genpath('..\..\OceanMesh2D'));
 addpath(genpath('..\..\m_map'));
 addpath(genpath('..\..\data'));
@@ -23,7 +28,7 @@ max_el_ns_all=[12e3 7e3 7e3 7e3];                                       % maximu
 R_all=[2 3 3 3];                                                        % nr of elements to solve the feature size of coastlines
 grade_all=[0.3 0.2 0.2 0.2];                                            % mesh grade
 dems=["gebco_azores_2020.nc"; "EMODNET_DTM_Azores_1.nc";
-        "EMODNET_DTM_Azores_2.nc"; "EMODNET_DTM_Azores_3.nc"];              % digital elevation model filenames, defining the input bathymetry
+        "EMODNET_DTM_Azores_2.nc"; "EMODNET_DTM_Azores_3.nc"];          % digital elevation model filenames, defining the input bathymetry
 coastlines=["GSHHS_h_L1"; "GSHHS_h_L1"; "GSHHS_h_L1"; "GSHHS_h_L1"];    % coastline filenames
 alpha_all = [0 25 25 25];                                               % alpha parameter to solve the slope mesh size function                                                        % filename of output mesh file (FLP) and prefix name of ENV files
 coord_source=[-29 38];                                                  % sound source coords in lat/lon
@@ -102,15 +107,15 @@ params_flp={title_flp calc_flp Nm Nsx coord_or_km(1) Nsy coord_or_km(2) NSz Sz .
 
 % Temperature-Salinity data extraction and arrangement
 fname_TSdata = 'global-analysis-forecast-phy-001-024-monthly_1656015476392.nc';
-month = 1; % month index (January)
 lon_TS=ncread(fname_TSdata,'longitude');
 lat_TS=ncread(fname_TSdata,'latitude');
 t_TS=ncread(fname_TSdata,'time')./24+datenum(1950,1,1)
-t_TS=t_TS(1); % extract the first instant
+idx_t = 1; % month index (data will be only extracted for January)
+t_TS=t_TS(idx_t); % extract the first instant
 T=ncread(fname_TSdata,'thetao');
-T=squeeze(T(:,:,:,month));
+T=squeeze(T(:,:,:,idx_t));
 S=ncread(fname_TSdata,'so');
-S=squeeze(S(:,:,:,month));
+S=squeeze(S(:,:,:,idx_t));
 z_TS=ncread(fname_TSdata,'depth');
 % concatenating the previous first depth level values of T and S to the zero level:
 if z_TS(1)~=0
@@ -118,8 +123,8 @@ if z_TS(1)~=0
     S=cat(3,S(:,:,month),S); 
     T=cat(3,T(:,:,month),T);
 end
-% Create a struct out of the T-S input data to be read in the
-% writekraken3d.m function
+
+% Create a struct out of the T-S input data to be read in OM2D's "write" function
 TS_data.S=S;
 TS_data.T=T;
 TS_data.lon=lon_TS;
@@ -135,6 +140,7 @@ TS_data.z=z_TS;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%% 3.1 geodata and edgefx %%%%%%%%%%
+
 for i=1:N
     min_el=min_el_all(i); max_el=max_el_all(i); max_el_ns=max_el_ns_all(i);
     R=R_all(i); grade=grade_all(i); alpha=alpha_all(i);
@@ -153,19 +159,18 @@ m=mshopts.grd;
 %%%%% 3.3 msh %%%%%%%%%%%%%%%%%%%%%%%%%
 
 m=interp(m,gdat);
-%m=make_bc(m,'auto',gdat{1}); 
 tri=m.t;
 lon=m.p(:,1);lat=m.p(:,2);
 z=m.b;
 
-% define Lat/Lon coords of origin the new referential in km for KRAKEN
+% Define Lat/Lon coords of the new referential's origin in km for KRAKEN
 [lo,la,idx] = find_nearest_point(lon, lat, coord_source(1), coord_source(2)); % source coordinates and index in the mesh
-coord_or = [lo la]; % coords of source (in degrees), which will be the origin of the referential in km coords
+coord_or = [lo la]; % Coords of source (in degrees), which will be the origin of the referential in km coords
 
 % other corrections to the bathymetry
-z(isnan(z))=0;  % convert nan nodes into zeros
-z(z<0)=0;       % convert negative numbers (land) into zeros
-m.b=z;          % apply correction to mesh
+z(isnan(z))=0;  % Convert nan nodes into zeros
+z(z<0)=0;       % Convert negative numbers (land) into zeros
+m.b=z;          % Apply correction to mesh
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%% end of Run OceanMesh2D %%%%%%%%%%
@@ -175,43 +180,44 @@ m.b=z;          % apply correction to mesh
 %%%%%%%% 4 - Write mesh data %%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-flag_env=1;
-flag_flp=0;
+flag_env=1;    % Flag to write/store ENV files (0 - no; 1 - yes)
+flag_flp=0;    % Flag to write/store FLP files (0 - no; 1 - yes)
 
 % Struct array with all ENV inputs
 inp_env.flag = flag_env;
 inp_env.params = params_env;
 inp_env.TS_data = TS_data;
-% Struct array with all FLP inputs
+
+% Struct array with all FLP inputs (not needed, except for the flag)
 inp_flp.coord_or = coord_or;
 inp_flp.flag = flag_flp;
 inp_flp.params = params_flp;
 fnames = {fname_env fname_flp};
 
+% Write data to ENV files for KRAKEN3D
 write(m,fnames,'kraken3d','nob',1,'inp_env',inp_env,'inp_flp',inp_flp);
 
-
-% write(m,fname,'kraken3d','nob',1,'fname_env',fname,'TS_data',TS_data, ...
-%     'params_env',params_env, 'coord_or',coord_or,'params_flp',params_flp, ...
-%     'varargin_kraken3d',[flag_env, flag_flp]);
-
+% Create a directory for input/output data of KRAKEN, if it doesnt exist
 if ~exist('data_kraken','dir')
-    mkdir('data_kraken')                        % create a direectory for input/output files of kraken if it doesnt exist
+    mkdir('data_kraken')
 else
-    delete('data_kraken\*')                     % remove everything from the input/ouput kraken directory
+    delete('data_kraken\*')    % Remove everything from the KRAKEN data directory
 end
 
-movefile('*.env','data_kraken\')                % move all envs to the input/ouput kraken directory
+% Move all envs to the input/ouput kraken directory
+movefile('*.env','data_kraken\')
 
+% Create a directory for plotting if it doesnt exist
 if ~exist('plots\','dir')
-    mkdir('plots\')                                 % create a directory for plotting if it doesnt exist
+    mkdir('plots\')
 end
 
+% Create a Struct with mesh data (bathymetry, nodes' position, etc.)
 mesh_data.z=z;
 mesh_data.lon=lon;
 mesh_data.lat=lat;
 mesh_data.tri=tri;
-save('mesh_data.mat','-struct','mesh_data');    % save a MATLAB struct with mesh info (to be used in Plotting_Azores.m)
+save('mesh_data.mat','-struct','mesh_data');    % Save the MATLAB struct with mesh info (to be used in Plotting_Azores.m)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%% end of Write mesh data %%%%%%%%%%
@@ -221,12 +227,14 @@ save('mesh_data.mat','-struct','mesh_data');    % save a MATLAB struct with mesh
 %%%%%%%% 5 - Plotting mesh %%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+view_tri=1;   % Flag to view mesh triangles
+view_cb=1;    % Flag to view colorbar
+
+% Plot the entire mesh (Azores archipelago)
 figure
-caxis_lims=[0 3e3]; % meters
-view_tri=1;view_cb=1;
+caxis_lims=[0 3e3]; % depth limits, in meters
 lonlat_lims=d_limits(1,:);
 plot_mesh_bathy(tri, z, lon, lat, lonlat_lims, caxis_lims,view_tri,view_cb)
-%plot3(lo,la,1e6,'ro','MarkerFaceColor','r')
 plot3([lonlat_lims(1) lonlat_lims(1) lonlat_lims(2) lonlat_lims(2) lonlat_lims(1)], ...
     [lonlat_lims(3) lonlat_lims(4) lonlat_lims(4) lonlat_lims(3) lonlat_lims(3)], ...
     ones(1,5).*1e6,'g-','LineWidth',2.5)
@@ -244,37 +252,37 @@ plot3([lonlat_lims(1) lonlat_lims(1) lonlat_lims(2) lonlat_lims(2) lonlat_lims(1
     ones(1,5).*1e6,'r-','LineWidth',2.5)
 saveas(gcf,['plots/' fname_env '_mesh_box1.png'])
 
+% Plot mesh in boundary box 2 (Western Group of Azores)
 figure
 caxis_lims=[0 1e3]
 view_cb=1;
 lonlat_lims=d_limits(2,:);
 view_tri=1;
 plot_mesh_bathy(tri, z, lon, lat, lonlat_lims, caxis_lims, view_tri, view_cb)
-% plot3(lo,la,1e6,'ro','MarkerFaceColor','r')
 plot3([lonlat_lims(1) lonlat_lims(1) lonlat_lims(2) lonlat_lims(2) lonlat_lims(1)], ...
     [lonlat_lims(3) lonlat_lims(4) lonlat_lims(4) lonlat_lims(3) lonlat_lims(3)], ...
     ones(1,5).*1e6,'r-','LineWidth',2.5)
 saveas(gcf,['plots/' fname_env '_mesh_box2.png'])
 
+% Plot mesh in boundary box 3 (Central Group of Azores)
 figure
 caxis_lims=[0 1e3]
 view_cb=1;
 lonlat_lims=d_limits(3,:);
 view_tri=1;
 plot_mesh_bathy(tri, z, lon, lat, lonlat_lims, caxis_lims, view_tri, view_cb)
-%plot3(lo,la,1e6,'ro','MarkerFaceColor','r')
 plot3([lonlat_lims(1) lonlat_lims(1) lonlat_lims(2) lonlat_lims(2) lonlat_lims(1)], ...
     [lonlat_lims(3) lonlat_lims(4) lonlat_lims(4) lonlat_lims(3) lonlat_lims(3)], ...
     ones(1,5).*1e6,'r-','LineWidth',2.5)
 saveas(gcf,['plots/' fname_env '_mesh_box3.png'])
 
+% Plot mesh in boundary box 4 (Eastern Group of Azores)
 figure
 caxis_lims=[0 1500]
 view_cb=1;
 lonlat_lims=d_limits(4,:);
 view_tri=1;
 plot_mesh_bathy(tri, z, lon, lat, lonlat_lims, caxis_lims, view_tri, view_cb)
-%plot3(lo,la,1e6,'ro','MarkerFaceColor','r')
 plot3([lonlat_lims(1) lonlat_lims(1) lonlat_lims(2) lonlat_lims(2) lonlat_lims(1)], ...
     [lonlat_lims(3) lonlat_lims(4) lonlat_lims(4) lonlat_lims(3) lonlat_lims(3)], ...
     ones(1,5).*1e6,'r-','LineWidth',2.5)
