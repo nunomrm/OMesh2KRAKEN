97b3f8fbf2714849ea1b631b74df68c99cde3af8
Nuno Rocha <101452182+nunomrm@users.noreply.github.com>
2022-12-18 17:49:57 +0000

    Update writekraken3d.m

--- OceanMesh2D/@msh/private/writekraken3d.m
+++ OceanMesh2D/@msh/private/writekraken3d.m
@@ -1,98 +1,102 @@
-function outname = writekraken3d(fnames,tri,VX,B,inp_env,inp_flp)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% INPUT:                                                    %
-% tri: triangle (nelem,3);                                  %
-% VX(:,1): longitude (nnode,1);                             %
-% VX(:,2): latitude (nnode,1);                              %
-% B: depth (nnode,1);                                       %
-% fnames: mesh name                                         %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function outname = writekraken3d(tri,VX,B,inp_env,inp_flp)
+%
+% (c) 2022 Nuno Monteiro, University of Aveiro
+%
+% Writes required input ENV and FLP files for KRAKEN and 
+% FIELD3D, from an OceanMesh2D unstructured mesh and given
+% temperature and salinity data for sound speed calculation
+% (the latter not required, and a constant value of sound
+% speed can be attributed to all ENV files)
+%
+%%% INPUTS
+% tri - triangular mesh elements from the mesh, with N_e 
+% elements: (N_e,3)
+% VX - mesh vertices (or nodes) coordinates (nnode,2)
+% B - bathymetry field (nnode, 1)
+% inp_env - struct with inputs for ENV file creation
+% inp_flp - struct with inputs for FLP file creation
+%
+%%% OUTPUTS:
+% outname - output filename
+%
 
-
+% number of nodes, longitude and latitude
 nnode=length(VX(:,1));
 lon=VX(:,1);
 lat=VX(:,2);
 
-flag_env = inp_env.flag;
-flag_flp = inp_flp.flag;
-fname_env = fnames{1};
-fname_flp = fnames{2};
+% extract flags for ENV/FLP file writing
+flag_env = inp_env.flag; % 0 - write ENV files, 1 - don't write ENV files
+flag_flp = inp_flp.flag; % 0 - write FLP file, 1 - don't write FLP file
 
-if flag_env==1
+if flag_env == 1
 
     disp('Writing ENV files')
-
+    
+    % extract struct fields from inp_env 
+    fname_env = inp_env.fname;
     params_env = inp_env.params;
-    TS_data = inp_env.TS_data;
-     
-    % T-S data extraction
-    lo_d=TS_data.lon;
-    la_d=TS_data.lat;
-    T=TS_data.T;
-    S=TS_data.S;
-    z_TS=TS_data.z;
-    x1=min(lon); x2=max(lon); y1=min(lat); y2=max(lat);
-    x_f=lo_d>=x1-0.1&lo_d<=x2+0.1;
-    y_f=la_d>=y1-0.1&la_d<=y2+0.1;
-    lo_d=lo_d(x_f);
-    la_d=la_d(y_f);
-    [LO_D,LA_D]=meshgrid(lo_d,la_d);
-    LO_D=LO_D';
-    LA_D=LA_D';
+    deltassp=params_env.deltassp;
+    z_fin = 0:deltassp:max(B);
+    cInt.Low = params_env.C_lim(1);
+    cInt.High = params_env.C_lim(2);
+    model = 'KRAKEN';
+    Receiverfin = params_env.Rdepth; 
+    sourcedepth = params_env.Sdepth;
+    freqT = params_env.freq;
+    SSP.NMedia = params_env.NMedia;
+    SSP.N =  params_env.N_layers;
+    SSP.sigma = params_env.sigma_int;
     
-    % Calculation of SSP in the unstructured mesh from T-S data
-    N=nnode;
-    Nz=length(z_TS);
-    Nx_d=length(lo_d);
-    Ny_d=length(la_d);
-    S_n=zeros(N,Nz);
-    T_n=zeros(N,Nz);
-    D_n=zeros(N,Nz);
-    SSP_n=zeros(N,Nz);
-
-    cInt.Low = params_env{end-3};
-    cInt.High = params_env{end-2};
-    model    = 'KRAKEN';
-    Receiverfin = params_env{8}; 
-    deltassp=params_env{end-1};
-    sourcedepth=params_env{end-5};
-    freqT    = params_env{1};
-    SSP.NMedia = params_env{2};
-    SSP.N      = [ params_env{6:7} ];
-    SSP.sigma  = [ params_env{4:5} ]; %Hrms
+    % Conditions for sound speed calculation or not (depending on presence of temperature/salinity input data)
+    if isfield(inp_env,'TS_data')==1 % activation of sound speed calculation
+        TS_data = inp_env.TS_data;
+        % Calculation of SSP in the unstructured mesh from T-S data
+        mesh.lon = lon;
+        mesh.lat = lat;
+        mesh.z = B;
+        mesh.tri = tri;
+        [SSP_fin, T, S] = calc_ss(TS_data, mesh, z_fin);
+        
+    else    % in case a sound speed constant value for all points (inp_env.SS_constant) or nothing is given
+        if isfield(inp_env,'SS_constant')==1
+            SSP_fin = ones(nnode,length(z_fin)).*inp_env.SS_constant;
+        else
+            SSP_fin = ones(nnode,length(z_fin)).*1500;
+        end
+    end
     
-    mesh.lon = lon;
-    mesh.lat = lat;
-    mesh.z = B;
-    mesh.tri = tri;
-    [SSP_fin,z_fin,T_fin,S_fin] = interp_ss(TS_data, mesh, deltassp);
-
-    for idx=1:nnode
-        envfil   = sprintf('%s_%06d',fname_env,idx);
+    % Water sound speed profiles (SSPs): adjustments
+    for idx = 1:nnode
+        envfil = sprintf('%s_%06d',fname_env,idx);
         TitleEnv = envfil;
-        zi=[0:deltassp:B(idx)];
-        len_z=length(zi);
-        SSP_idx=SSP_fin(idx,1:len_z);
         
-        %Water
-
-        if round(10.^2*zi(len_z))/10.^2~=round(10.^2*B(idx))/10.^2
-            zi(end+1)=B(idx);
-            SSP_idx(end+1)=[SSP_idx(len_z)];
-        end
-
-        if len_z==1
-            zi(2)=deltassp;
-            SSP_idx(2)=SSP_idx(1);
+        zi = 0:deltassp:B(idx);
+        len_z = length(zi);
+        SSP_idx = SSP_fin(idx,1:len_z);        
+        if zi(len_z) ~= B(idx) && len_z>1 % condition to insert the bathymetry at each node as the final depth and attribute 
+                                        % the sound speed value of the deepest point
+            zi(end+1) = B(idx);
+            SSP_idx(end+1) = SSP_idx(len_z);
+        elseif len_z == 1 && zi < deltassp  % condition to assure two depth levels in the water columns of ENV files
+                                            % (if there is one depth, KRAKEN returns error)
+            if zi(1) > 1e-2
+                zi = [0 B(idx)];
+                SSP_idx = [SSP_idx(1) SSP_idx(1)];
+            else
+                zi = [0 1e-2];
+                SSP_idx = [SSP_idx(1) SSP_idx(1)];
+            end
         end
         
-        if isnan(SSP_idx(2))==1
-            SSP_idx(2)=SSP_idx(1);
+        if str2num(sprintf('%.2f',zi(end)))==str2num(sprintf('%.2f',zi(end-1)))
+            zi(end) = [];
+            SSP_idx(end) = [];
         end
+        
+        % create adequate struct files to save ENV files with the help of write_env (function from the Acoustic's Toolbox)
         WalphaR=SSP_idx;     
         SSP.depth  = [0 zi(end) Receiverfin];        
-        
-
         WbetaR=ones(size(zi,2),1)*0;
         Wrho=ones(size(zi,2))*1;
         WalphaI=ones(size(zi,2))*0;
@@ -105,7 +109,7 @@ if flag_env==1
         SSP.raw( 1 ).betaI  = WbetaI;                  
         %Bottom
         SSP.raw( 2 ).z      = [ zi(end) Receiverfin ];
-        SSP.raw( 2 ).alphaR = [ params_env{end} params_env{end} ]; % speed
+        SSP.raw( 2 ).alphaR = params_env.C_bot;
         Bdry.Bot.HS.alphaR  = 0;
         SSP.raw( 2 ).betaR  = [ 0   0 ];
         Bdry.Bot.HS.betaR= 0;
@@ -117,7 +121,7 @@ if flag_env==1
         Bdry.Bot.HS.betaI= 0 ;  
         Bdry.Top.Opt   = 'CVW .'; %S-cubic spline interpolation, C-C-linear interpolation, N-N2-linear interpolation. 
         Bdry.Bot.Opt   = 'V'; %V vacuum bellow
-        Bdry.Bot.sigma = params_env{end};    
+        Bdry.Bot.sigma = params_env.sndspd_bot;    
         RMax        = 0;
         Pos.s.z = sourcedepth;
         Pos.r.z = 0:deltassp:Receiverfin;      
@@ -126,20 +130,22 @@ if flag_env==1
     end
     
 end
-%% write flp
 
 if flag_flp==1 
 
-    disp('Writing FLP files')
+    disp('Writing the FLP file')
     
+    % load data from inp_flp struct
+    fname_flp = inp_flp.fname;
     params_flp = inp_flp.params;
     coord_or = inp_flp.coord_or;
     
-    wgs84 = wgs84Ellipsoid; % reference ellipsoid
     coord_km=zeros(nnode,2);
-    coord_or_km = [params_flp{5} params_flp{7}];
+    coord_or_km = params_flp.coord_or;
+    
+    % conversion of mesh node coordinates from lon/lat (degrees) to km
     for i = 1:length(lon)
-        [arclen,az] = distance(coord_or(2),coord_or(1),lat(i),lon(i),wgs84); % arclen in m, az in deg
+        [arclen,az] = distance(coord_or(1,2),coord_or(1,1),lat(i),lon(i),wgs84Ellipsoid); % arclen in m, az in deg
         arclen = arclen/1e3; % convert to km
         % convert azimuth angles to normal angles
         theta = 450 - az;
@@ -148,45 +154,57 @@ if flag_flp==1
         coord_km(i,:) = [arclen*cosd(theta) arclen*sind(theta)]-coord_or_km;
     end
     
+    % write to FLP file
     fid = fopen([fname_flp '.flp'],'w');
-
-    outname = sprintf("'%s'",fname_flp) ;
-    fprintf(fid,"'%s' 				! TITLE\n" + ...
-        "'%sFM'  				! OPT\n" + ...
-        "%i  	    		! NUMBER OF MODES\n" + ...
-        "%i                      ! Nsx number of source coordinates in x\n" + ...
-        "%.4f /  			    ! x coordinate of source (km)\n" + ...
-        "%i                      ! Nsy number of source coordinates in y\n" + ...
-        "%.4f /  				! y coordinate of source (km)\n" + ...
-        "%i                      ! NSz\n" + ...
-        "%.1f, /				! Sz( 1 : NSz ) (m)\n" + ...
-        "%i                    ! NRz\n" + ...
-        "%.1f %.1f /		! Rz( 1 : NRz ) (m)\n" + ...
-        "%i                ! NRr\n" + ...
-        "%.1f %.1f / 		! Rr( 1 : NRr ) (km)\n" + ...
-        "%i                   ! Ntheta\n" + ...
-        "%.1f %.1f /              ! theta( 1 : Ntheta) (degrees)\n", ...
-        params_flp{1}, params_flp{2}, [params_flp{3:end-6}]);
+    outname = sprintf("'%s'",fname_flp);
+    fprintf(fid,"'%s'    ! TITLE\n" + ...
+        "'%sFM' ! OPT\n" + ...
+        "%i ! NUMBER OF MODES\n" + ...
+        "%i ! Nsx number of source coordinates in x\n" + ...
+        "%.2f %.2f / ! x coordinate of source (km)\n" + ...
+        "%i ! Nsy number of source coordinates in y\n" + ...
+        "%.2f %.2f / ! y coordinate of source (km)\n" + ...
+        "%i    ! NSz\n" + ...
+        "%.1f %.1f / ! Sz( 1 : NSz ) (m)\n" + ...
+        "%i ! NRz\n" + ...
+        "%.1f %.1f / ! Rz( 1 : NRz ) (m)\n" + ...
+        "%i ! NRr\n" + ...
+        "%.2f %.2f / ! Rr( 1 : NRr ) (km)\n" + ...
+        "%i ! Ntheta\n" + ...
+        "%.1f %.1f / ! theta(1 : Ntheta) (degrees)\n", ...
+        params_flp.title, params_flp.calc, params_flp.Nm, ...
+        params_flp.Ns(1), params_flp.Sxy_lim(1,1), ...
+        params_flp.Sxy_lim(1,2), params_flp.Ns(2), ...
+        params_flp.Sxy_lim(2,1), params_flp.Sxy_lim(2,2), ... %
+        params_flp.NSz, params_flp.Sz_lim(1), ...
+        params_flp.Sz_lim(2), params_flp.NRz, ...
+        params_flp.Rz_lim(1), params_flp.Rz_lim(2), ...
+        params_flp.NRr, params_flp.Rr_lim(1), ...
+        params_flp.Rr_lim(2), params_flp.NRtheta, ...
+        params_flp.Rtheta_lim(1), params_flp.Rtheta_lim(2));
     
-    fprintf(fid,'%d					! Number of nodes\n', nnode);
+    fprintf(fid,'%d    ! NUMBER OF NODES\n', nnode);
     for i=1:nnode
-        fprintf(fid,["%5.5f, %5.5f, '%s_%06d'\n"], coord_km(i,1),coord_km(i,2),fname_env,i);
+        fprintf(fid,"%5.5f, %5.5f, '%s_%06d'\n", coord_km(i,1),coord_km(i,2),fname_env,i);
     end
     m=0;
-    fprintf(fid,'%d, 					NUMBER OF ELEMENTS (TRIANGLES)\n',length(tri(:,1)));
+    fprintf(fid,'%d !    NUMBER OF ELEMENTS (TRIANGLES)\n',length(tri(:,1)));
     for i=1:length(tri(:,1))
         m=m+1;
         if rem(i,5)~=0
-            fprintf(fid,['%d, %d, %d,\n'], tri(i,1),tri(i,2),tri(i,3));
+            fprintf(fid,'%d %d %d\n', tri(i,1),tri(i,2),tri(i,3));
         else
-            fprintf(fid,['%d, %d, %d, #%d\n'], tri(i,1),tri(i,2),tri(i,3),i);  
+            fprintf(fid,'%d %d %d #%d\n', tri(i,1),tri(i,2),tri(i,3),i);  
         end
     end
     
-    fprintf( fid, "%.2f %.2f %i          /   ! ALPHA1  ALPHA2  NALPHA\n" + ... 
-                "%.3f %i          /   ! STEP  NSTEPS\n" + ... 
-                "%.3f          /   ! EPMULT", ...
-                [params_flp{end-5:end}]);
+    fprintf( fid, "%.2f %.2f %i          /    ! ALPHA1  ALPHA2  NALPHA\n" + ... 
+                "%.3f %i          /    !    STEP  NSTEPS\n" + ... 
+                "%.3f          /    ! EPMULT", ...
+                params_flp.GBtheta_lim(1), params_flp.GBtheta_lim(2), ...
+                params_flp.NGBtheta, params_flp.GBstep, ...
+                params_flp.GBsteps, params_flp.epsilon_mult);
+
     fclose(fid);
 end
 
