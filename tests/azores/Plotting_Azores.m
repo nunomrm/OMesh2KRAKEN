0a1b526436de5971b56a57fdd0a3e410753daca8
Nuno Rocha <101452182+nunomrm@users.noreply.github.com>
2022-12-18 22:30:55 +0000

    Update Plotting_Azores.m

--- tests/azores/Plotting_Azores.m
+++ tests/azores/Plotting_Azores.m
@@ -27,14 +27,10 @@ tri=mesh.tri;
 xy_lims=[min(lon_m) max(lon_m) min(lat_m) max(lat_m)];
 
 % Initialize Cg matrix for first 10 modes
-N_mod=zeros(N,1);                   % array of the number of modes of all prt files
-N_prt_mod=zeros(N,1);               % array of the printed nr of modes of all prt files
-m=10;                               % number of modes to extract
-Cg = zeros(N,m);                    % matrix of group velocities obtained by KRAKEN (first m modes at each point are extracted)
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%% end of pre-processing %%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+N_mod = zeros(N,1); % array of the number of modes of all prt files
+N_prt_mod = zeros(N,1); % array of the printed nr of modes of all prt files
+m=10; % number of modes to extract
+Cg = zeros(N,m); % matrix of group velocities obtained by KRAKEN (first m modes at each point are extracted)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Plotting Group Velocity (Cg) %%
@@ -43,16 +39,16 @@ Cg = zeros(N,m);                    % matrix of group velocities obtained by KRA
 for i=1:N
     fname = [dirname fname_env '_' sprintf( '%06d', i ) '.prt'];
     fid = fopen(fname);
-    lines=textread(fname,'%s','delimiter','\n');
-    i_m=find(~cellfun(@isempty,strfind(lines,'Number of modes')));
-    N_mod(i)=str2num(char(regexp(char(lines(i_m)),'\d*','Match')));     % number of modes
-    i_0=find(~cellfun(@isempty,strfind(lines,'I    k (1/m)')))+1;
-    i_f=find(~cellfun(@isempty,strfind(lines,'_')))-1;
-    N_prt_mod(i)=length(i_0:i_f(end));                                  % number of printed modes
-    lin=lines(i_0:i_f(end));                                            % lines for cg extraction
-    for j=1:m
-        vals=split(char(lin(j)));       % array of values in each column
-        Cg(i,j)=str2num(char(vals(5))); % extract fifth column and store in the Cg matrix
+    lines = textread(fname,'%s','delimiter','\n');
+    i_m = find(~cellfun(@isempty,strfind(lines,'Number of modes')));
+    N_mod(i) = str2num(char(regexp(char(lines(i_m)),'\d*','Match'))); % number of modes
+    i_0 = find(~cellfun(@isempty,strfind(lines,'I    k (1/m)')))+1;
+    i_f = find(~cellfun(@isempty,strfind(lines,'_')))-1;
+    N_prt_mod(i) = length(i_0:i_f(end)); % number of printed modes
+    lin = lines(i_0:i_f(end)); % lines for cg extraction
+    for j = 1:m
+        vals = split(char(lin(j))); % array of values in each column
+        Cg(i,j) = str2num(char(vals(5))); % extract fifth column and store in the Cg matrix
     end
     fclose(fid);
 end
@@ -66,7 +62,3 @@ fill3(lo_s,la_s,ones(1,length(lo_s)).*1e6,'k')
 xlim([xy_lims(1) xy_lims(2)])
 ylim([xy_lims(3) xy_lims(4)])
 saveas(gcf,['plots/Cg_mode_1.png'])
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%% end of plotting Cg %%%%5%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
